b2cd8f8576e3b8f1ce4cdcbc25bf0f4100ea3d25
Maitrey Pandya <maitrey@Maitreys-MacBook-Air.local>
2024-12-02 19:36:07 +0530

    The is for making frontend work

--- backend1/app.js
+++ backend1/app.js
@@ -17,7 +17,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-    origin: ['https://book-rent-try.onrender.com', 'http://localhost:3000','*'],
+    origin: ['https://book-rent-try.onrender.com', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 
