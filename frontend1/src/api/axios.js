d12e7ab514859f0f4e30daedce0ca6a77969c5f0
Maitrey Pandya <maitrey@Maitreys-MacBook-Air.local>
2024-12-02 18:04:34 +0530

    The is for making frontend work

--- frontend1/src/api/axios.js
+++ frontend1/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
+  baseURL: 1
     ? 'https://book-rent-try-backend.onrender.com'
     : 'http://localhost:5001',
   withCredentials: true,
