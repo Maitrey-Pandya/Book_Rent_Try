c4c0e79d1918e97046f15ed4d17307faaedb7fa1
Maitrey Pandya <maitrey@Maitreys-MacBook-Air.local>
2024-12-02 21:06:16 +0530

    Change only in app.js

--- backend1/app.js
+++ backend1/app.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json({ limit: '10kb' }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: '*',//process.env.FRONTEND_URL || 'http://localhost:3000' ||,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin','Access-Control-Allow-Credentials','Access-Control-Allow-Headers'],
